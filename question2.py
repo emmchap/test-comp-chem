fe0f3129a611e49b76a1e9f5374a09199f40009a
Emmanuel Chaplais <chaplais.emmanuel@gmail.com>
2020-10-14 16:56:16 +0200

    Question 2 answer

---
+++ question2.py
@@ -0,0 +1,10 @@
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors
+
+suppl = Chem.SDMolSupplier('input/824_ideal.sdf')
+for mol in suppl:
+    print("Number of rotatable bonds:", rdMolDescriptors.CalcNumRotatableBonds(mol))
+    print("Number of rings:", rdMolDescriptors.CalcNumRings(mol))
+    print("Number of acceptor atoms:", rdMolDescriptors.CalcNumHBA(mol))
+    print("Number of donor atoms:", rdMolDescriptors.CalcNumHBD(mol))
+    print("Wildman-Crippen LogP:", rdMolDescriptors.CalcCrippenDescriptors(mol)[0])
