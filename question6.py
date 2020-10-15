7e400aa9d3bdafa2ed22a450cbdc89fc493fa81f
Emmanuel Chaplais <chaplais.emmanuel@gmail.com>
2020-10-15 15:42:01 +0200

    multiple radius and number of bits

--- question6.py
+++ question6.py
@@ -1,11 +1,15 @@
 from rdkit import Chem, DataStructs
-from rdkit.Chem import AllChem
+from rdkit.Chem import AllChem, Draw
 
 m1 = Chem.MolFromSmiles('CC(C)NCC(O)COC1=CC=CC2=CC=CC=C12')
 m2 = Chem.MolFromSmiles('CC(C)NS(=O)(=O)CCOC1=CC=CC2=CC=CC=C12')
 
-fp1 = AllChem.GetMorganFingerprint(m1,2)
-fp2 = AllChem.GetMorganFingerprint(m2,2)
+Rads = [1,2,3,4]
+Bits = [512,1024,2048,4096]
 
-MolSim = DataStructs.TanimotoSimilarity(fp1,fp2)
-print(MolSim)
+for Rad in Rads:
+    for Bit in Bits:
+        fp1 = AllChem.GetMorganFingerprintAsBitVect(m1, Rad, nBits=Bit)
+        fp2 = AllChem.GetMorganFingerprintAsBitVect(m2, Rad, nBits=Bit)
+        MolSim = DataStructs.TanimotoSimilarity(fp1, fp2)
+        print("Radius of ", Rad, ", ", Bit, " bits: ", MolSim,sep="")
